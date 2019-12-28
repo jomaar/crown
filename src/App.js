d462b1786f1afa1ff28327d4067106081b75cbee
jomaar <jomaargmx.de>
2019-12-28 09:35:08 +0100

    homepage broken into small components, menu-item component styled

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 
-import HomePage from './homapage.component';
+import HomePage from './pages/homepage/homapage.component';
 
 function App() {
   return (
